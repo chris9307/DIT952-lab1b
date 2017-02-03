95ff2645ad1f17289132fdc968612ed0fa37d159
X <Rapier94@hotmail.se>
2017-02-03 09:00:25 +0100

    Scania

--- Scania.java
+++ Scania.java
@@ -1,4 +1,7 @@
 package dit952.lab.b;
+
+import java.awt.Color;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,20 +14,43 @@ package dit952.lab.b;
  * @author Asus
  */
 public class Scania extends Car{
-
+    private Platform platform=new Platform();
+    public Scania(){
+        nrDoors = 2;
+        color = Color.black;
+        enginePower = 125;
+	    turboOn = false;
+        modelName="Scania";
+    }
     @Override
     public double speedFactor() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public void incrementSpeed(double amount) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /**Decreases the angle as long as the current angle os within the range [1,70]*/
+    public void decreaseAngle(){
+        if(platform.getAngle()==0)
+        {
+            
+        }
+        else
+        {
+         platform.decreaseAngle();   
+        }     
     }
-
-    @Override
-    public void decrementSpeed(double amount) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    /**Decreases the angle as long as the current angle os within the range [0,69]*/
+        public void increaseAngle(){
+        if(platform.getAngle()==70)
+        {
+            
+        }
+        else
+        {
+         platform.increaseAngle();   
+        }     
     }
+    
+    
     
 }
