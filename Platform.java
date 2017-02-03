4a002f421952c017192bd468a9c92ab84c2adff0
X <Rapier94@hotmail.se>
2017-02-03 08:41:32 +0100

    Platform Class added

---
+++ Platform.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dit952.lab.b;
+
+/**
+ *
+ * @author Asus
+ */
+public class Platform {
+    private int angle;
+    /***/
+    public int getAngle(){
+        return angle;
+    }
+    /**Decreases the current angle by one*/
+    public void decreaseAngle(){
+        this.angle--;
+    }
+    /**Increases the current angle by one*/
+    public void increaseAngle(){
+        this.angle++;
+    }
+    /**Sets the angle to the maximum allowed degree 70*/
+    public void setMaxAngle(){
+        this.angle=70;
+    }
+    /**Sets the angle to the maximum allowed degree 0*/
+    public void setMinAngle(){
+        this.angle=0;
+    }
+    
+}
